58b13d72244669301f5e277bcbacc289c74c3ea7
aopecho <aopecho@uchicago.edu>
2022-04-19 23:11:26 +0000

    just seeing what i remember of rcav, while i'm at it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,48 @@
 class ApplicationController < ActionController::Base
+  def homepage
+    render({  :template => "game_templates/rules.html.erb"  })
+  end
+
+  def play_rock 
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    elsif @comp_move == "scissors"
+      @ouctome = "won"
+    end
+    
+    render({  :template => "game_templates/user_rock.html.erb  "})
+  end
+
+  def play_paper 
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @ouctome = "lost"
+    end
+    
+    render({  :template => "game_templates/user_paper.html.erb  "})
+  end
+
+  def play_scissors 
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @ouctome = "tied"
+    end
+    
+    render({  :template => "game_templates/user_scissors.html.erb  "})
+  end
+
 end
